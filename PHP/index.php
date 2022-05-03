770cdcd3aeb95d943f87ecea7642fdafb6e03384
Angel Banze <angeleliasanjo@gmail.com>
2022-05-03 05:48:55 -0700

    starting

---
+++ PHP/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-pt">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Site pessoal</title>
+
+    <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
+</head>
+<body>
+    
+    <script>
+    </script>
+</body>
+</html>
